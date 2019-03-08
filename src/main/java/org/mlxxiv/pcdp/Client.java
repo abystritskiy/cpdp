89efeaf92496aea63b18fcb4e80cb334f4c1319f
Alex Bystritskiy <alex.bystritskiy@gmail.com>
2019-03-08 14:36:45 +0100

    Week 1 + additional init

---
+++ src/main/java/org/mlxxiv/pcdp/Client.java
@@ -0,0 +1,14 @@
+package org.mlxxiv.pcdp;
+
+import static edu.rice.pcdp.PCDP.async;
+import static edu.rice.pcdp.PCDP.finish;
+
+public class Client {
+    public static void main(String[] args) {
+        async(()->{
+
+        });
+//        consequent();
+//        paralel();
+    }
+}
